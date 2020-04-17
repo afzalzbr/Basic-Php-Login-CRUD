070fd3bea5aa0cc32ce6cb13c8d1ba0a79d0c556
Afzal Zubair <afzal.zubair@khi.iba.edu.pk>
2020-04-17 16:47:03 +0500

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 
 
@@ -7,20 +8,11 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Navbar dark color="primary">
+        <div className="contianer">
+          <NavbarBrand href="/">Ristoreante Con Fusion</NavbarBrand>
+        </div>
+        </Navbar>
       </div>
     );
   }
